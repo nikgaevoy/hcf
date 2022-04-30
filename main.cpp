8720fbe627ef781deba578042b1681d90c679447
Nikita Gaevoy <nikgaevoy@gmail.com>
2022-04-30 20:21:11 +0300

    Problem

--- main.cpp
+++ main.cpp
@@ -2,9 +2,80 @@
 
 using namespace std;
 
+using ll = long long;
+
+using pnt = complex<int>;
+
+ll operator&(const pnt &a, const pnt &b)
+{
+	return a.real() * ll(b.real()) + a.imag() * ll(b.imag());
+}
+
+ll len2(const pnt &x)
+{
+	return x & x;
+}
+
+struct Problem
+{
+	struct gift
+	{
+		int id{}, score{}, weight{};
+		pnt pos;
+	};
+
+	int tl, d;
+	vector<int> l, a;
+	unordered_map<string, int> who;
+	vector<string> names;
+	vector<gift> gifts;
+
+	explicit Problem(istream &cin = std::cin)
+	{
+		int w, g;
+
+		cin >> tl >> d >> w >> g;
+
+		l.resize(w);
+		a.resize(w);
+		names.resize(g);
+		who.reserve(g);
+		gifts.resize(g);
+
+		for (auto &it: l)
+			cin >> it;
+		for (auto &it: a)
+			cin >> it;
+
+		for (int i = 0; i < g; i++)
+		{
+			cin >> names[i];
+			who[names[i]] = i;
+
+			int score, weight;
+			int x, y;
+
+			cin >> score >> weight >> x >> y;
+
+			gifts[i] = {i, score, weight, pnt{x, y}};
+		}
+
+		assert(cin.eof());
+	}
+};
+
+constexpr array dx = {0, 0, -1, 1}, dy = {1, -1, 0, 0};
+
+enum moves
+{
+	AccUp = 0, AccDown = 1, AccLeft = 2, AccRight = 3, Float, LoadCarrots, LoadGift, DeliverGift
+};
+
 int main()
 {
-	cout << "heol wrld" << endl;
+	ifstream ex("../a_an_example.in.txt");
+
+	Problem prb(ex);
 
 	return 0;
 }
