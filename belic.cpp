534a261964ff00e3fb31d85ba3664969e21926d6
Dmitriy Belichenko <mitterr1999@yandex.ru>
2022-04-30 19:46:12 +0300

    Create belic.cpp

---
+++ belic.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ld long double
+mt19937 rnd(228);
+uniform_int_distribution<int> rd(1, 1000000);
+int main() {
+    ios_base::sync_with_stdio(false);
+    
+    cin.tie(nullptr);
+    return 0;
+}
